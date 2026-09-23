5d1fee5c883aeb0efde0043e2bb477932a0e24ca
Ellen <ellen.j.hill@outlookcom>
2026-09-23 14:38:10 -0700

    Feat: created files for side by side work & created dice loop

---
+++ Farkle.java
@@ -0,0 +1,34 @@
+public class Main{
+public static void main (String[] args){
+    System. out.println("Farkle Rolling & Scoring in Java!");
+
+    int[] dice = {0,0,0,0,0,0};
+    for (int i = 1; i<=6; i++){
+        System. out.println(i);
+        System. out.println("successful run!");
+    }
+    //int randomInRange = (rand.nextInt%6)+1;
+    }
+}
+
+/*
+    int age = 30;
+    float price = 10.99F; //Suffix is "F" to ensure that it is a float
+    char letter = 'a'; // Suffix is to use single quotes, and double quotes are for strings
+    System. out.println(age);
+    System. out.println(price);
+    System. out.println(letter);
+
+// Escape Sequences
+System.out.println("\tIt's a \"beautiful\" day");
+
+// Arrays
+int[] myNum = {10,20,30,40};
+// Array Indexes start at 0, and 1 is the second element
+System.out.println(myNum[0]);
+
+//Multidimensional arrays!
+int[] [] myNums = {{1,2},{3,4,5},{6,7,8,9}};
+System.out.println(myNums[0][0]);
+System.out.println(myNums[1][2]);
+}*/
